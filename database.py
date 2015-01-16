1da6eeaaa552579a0ab3a2e7a77a49ebbac7c196
Leland Batey <lelandbatey@gmail.com>
2015-01-15 16:05:38 -0800

    Wrote new backend using Sqlite3

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import datetime
+import sqlite3
 import os.path
 import random
 import json
@@ -67,10 +68,13 @@ class ConfigReader(object):
             panic("Config file must specify password.") 
         if not 'buckets' in c:
             panic("Config file must specify at least one bucket (viewable directory).")
+        if not 'cache' in c:
+            panic("Config file must specify a cache directory.")
 
         self.username = c['username']
         self.password = c['password']
         self.buckets = c['buckets']
+        self.cache = c['cache']
         
 
 class FileEntry(object):
@@ -142,11 +146,137 @@ class Database(object):
 
 
 
+
+class SqliteDatabase(object):
+    """Database for holding the file information. Uses Sqlite3 as backend."""
+    def __init__(self, db_name="links_database.sqlite3"):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS files (
+            file_id         TEXT,
+            file_location   TEXT,
+            expiration_date TEXT,
+            download_count  INTEGER,
+            "timestamp" TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')),
+            UNIQUE(file_id)
+        )""")
+
+    def convert_results(self, results, fields):
+        """Maps results of query into list of dict-as-objects."""
+        out = []
+        for result in results:
+            tmp = {
+                fields[index]: result[index] for index in range(len(fields))
+            }
+            out.append(tmp)
+        return out
+
+    def results_to_entry(self, results):
+        """Converts SQL results into a list of entries-as-dictionary objects."""
+        return self.convert_results(results, [
+            'file_id', 'file_location', 'expiration_date',
+            'download_count', 'timestamp'
+        ])
+    
+    def fetch_entry(self, file_id):
+        """Returns raw_database results for file entry, if it exists."""
+        self.cursor.execute("SELECT * FROM files WHERE file_id=?", (file_id,))
+        out = self.cursor.fetchall()
+        out = self.results_to_entry(out)
+
+        if len(out) > 1:
+            raise LookupError(\
+                "LookupError: Multiple entries with the same file_id exist.")
+        if out:
+            return out[0]
+        else:
+            return []
+
+    def collate_entry(self, raw_entry):
+        """
+        Formats a `raw_entry` (output of fetching from the database) into a
+        more useful object.
+        """
+        tmp = dict(raw_entry)
+        tmp['file_exists'] = os.path.isfile(tmp['file_location'])
+
+        expiration_date = float(tmp['expiration_date'])
+        expiration_date = epoch_to_datetime(expiration_date)
+        if expiration_date < datetime.datetime.now():
+            tmp['is_expired'] = True
+        else:
+            tmp['is_expired'] = False
+        return tmp
+
+    def get_entry(self, file_id):
+        """Returns collated file entry, if it exists."""
+        entry = self.fetch_entry(file_id)
+        if entry:
+            entry = self.collate_entry(entry)
+
+        return entry
+
+    def entry_for_file(self, location):
+        """Given a file location, checks if an entry for that file exists."""
+        self.cursor.execute(\
+            "SELECT * FROM files WHERE file_location=?",\
+            (location,))
+        out = self.cursor.fetchall()
+        print(location)
+        jp(out)
+        if not out:
+            return False
+        else:
+            return True
+
+    def new_entry(self, location, expiration_delta=1):
+        """Creates entry for the new file in the database, returning its id."""
+
+        file_id = random_string()
+
+        now = datetime.datetime.now()
+        expiration_date = now + datetime.timedelta(days=expiration_delta)
+        expiration_date = datetime_to_epoch(expiration_date)
+
+        self.cursor.execute("""
+            INSERT INTO files 
+                (file_id, file_location, expiration_date, download_count)
+            VALUES (?,?,?,?)""", (file_id, location, expiration_date, 0))
+        self.connection.commit()
+        return file_id
+
+    def remove_entry(self, file_id):
+        """Removes the specified file entry."""
+        if self.get_entry(file_id):
+            print("Deleting file with id:", file_id)
+            self.cursor.execute("DELETE FROM files WHERE file_id=?", (file_id,))
+            self.connection.commit()
+
+    def to_dict(self):
+        """Returns a dict object representing the DB."""
+        self.cursor.execute("SELECT * FROM files")
+        out = self.cursor.fetchall()
+        out = self.results_to_entry(out)
+
+        return {x['file_id']: self.collate_entry(x) for x in out}
+
+
+
+
+
 def main():
     """Testing the classes."""
-    d = Database()
+    # d = Database()
+    d = SqliteDatabase()
 
-    d.new_entry('/tmp/some_file', 1)
+    test_file = "/tmp/some_file"
+
+    d.new_entry(test_file, 1)
+    dump = d.to_dict()
+    for x in dump:
+        if dump[x]['file_location'] == test_file:
+            d.remove_entry(x)
     jp(d.to_dict())
 
 
